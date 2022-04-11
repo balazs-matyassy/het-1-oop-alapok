f02a1542d5155848d0ba0d10656348606e6ed33d
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-04-11 19:02:58 +0200

    mb: updates

--- src/main/java/hu/progmatic/Lesson_20220411_03.java
+++ src/main/java/hu/progmatic/Lesson_20220411_03.java
@@ -2,6 +2,27 @@ package hu.progmatic;
 
 public class Lesson_20220411_03 {
     public static void main(String[] args) {
+        Travel moonTravel = new Travel();
+        moonTravel.from = "Föld";
+        moonTravel.to = "Hold";
+        moonTravel.time = 100;
+        moonTravel.price = 1000;
 
+        Travel marsTravel = new Travel("Föld", "Mars", 350, 3500);
+
+        travelData(moonTravel);
+        travelData(marsTravel);
+    }
+
+    public static void travelData(Travel travel) {
+        System.out.println("=========================");
+        System.out.println("Indulás: " + travel.from);
+        System.out.println("Cél: " + travel.to);
+        System.out.println("Hossz: " + travel.time);
+        System.out.println("Ár: " + travel.price);
+        System.out.println();
+
+        travel.price += 10; // visszaíródik
+        travel = null; // NEM íródik vissza
     }
 }
