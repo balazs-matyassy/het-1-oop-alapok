fb76606aab262e14f382a6b3d8919db326404c57
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-04-21 18:42:24 +0200

    mb: updates

---
+++ src/main/java/hu/progmatic/travel/ships/Spaceship.java
@@ -0,0 +1,51 @@
+package hu.progmatic.travel.ships;
+
+public class Spaceship {
+    private String type;
+    private int speed;
+    private int w, h, l;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public void setW(int w) {
+        this.w = w;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
+    public int getL() {
+        return l;
+    }
+
+    public void setL(int l) {
+        this.l = l;
+    }
+
+    public int getVolume() {
+        return w * h * l;
+    }
+}
