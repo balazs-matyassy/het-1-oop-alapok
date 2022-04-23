5ee4c2a4b236786d394a9fdab54b218e19206308
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-04-23 15:21:24 +0200

    mb: updates

---
+++ src/main/java/hu/progmatic/tourism_04_tostring/App.java
@@ -0,0 +1,24 @@
+package hu.progmatic.tourism_04_tostring;
+
+public class App {
+    public static void main(String[] args) {
+        Travel.setPresident("Gyula");
+        Travel.elect("Balázs", "Ria", 2000, 3000);
+
+        Planet[] planets = {
+                new Planet("001", "Föld", 0, 0, 0),
+                new Planet("002", "Mars", 251_000_000, 0, 0),
+                new Planet("003", "Hold", 384_400, 0, 0)
+        };
+        Travel[] travels = {
+                new Travel(planets[0], planets[1], 3.5, null),
+                new Travel(planets[1], planets[2], 0.5, 250),
+                new Travel(planets[2], planets[0], 3.0, 750)
+        };
+
+        for (Travel travel : travels) {
+            System.out.println(travel.getInfoSheet());
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/hu/progmatic/tourism_04_tostring/Travel.java
@@ -0,0 +1,100 @@
+package hu.progmatic.tourism_04_tostring;
+
+public class Travel {
+    public static final int FOUNDED = 2210;
+    private static String president;
+    private Planet source;
+    private Planet destination;
+    private double time;
+    private Integer price;
+
+    public Travel() {
+    }
+
+    public Travel(Planet source, Planet destination, double time, Integer price) {
+        this.source = source;
+        this.destination = destination;
+        this.time = time;
+        this.price = price;
+    }
+
+    public static void elect(
+            String candidate1, String candidate2, String candidate3,
+            int votes1, int votes2, int votes3
+    ) {
+        if (votes1 >= votes2 && votes1 >= votes3) {
+            president = candidate1;
+        } else if (votes2 >= votes3) { // votes2 >= votes1 && votes2 >= votes3
+            president = candidate2;
+        } else {
+            president = candidate3;
+        }
+    }
+
+    public static void elect(String candidate1, String candidate2, int votes1, int votes2) {
+        elect(candidate1, candidate2, null, votes1, votes2, Integer.MIN_VALUE);
+    }
+
+    public static String getPresident() {
+        return president;
+    }
+
+    public static void setPresident(String president) {
+        Travel.president = president;
+    }
+
+    public Planet getSource() {
+        return source;
+    }
+
+    public void setSource(Planet source) {
+        this.source = source;
+    }
+
+    public Planet getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Planet destination) {
+        this.destination = destination;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public double getDistance() {
+        double dx = source.getX() - destination.getX();
+        double dy = source.getY() - destination.getY();
+        double dz = source.getZ() - destination.getZ();
+
+        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    }
+
+    public String getInfoSheet() {
+        String priceInfo = price != null
+                ? "ár:\t\t\t" + price + " GFt"
+                : "JELENLEG NEM FOGLALHATÓ";
+
+        return "==============================\n"
+                + "\t Alapítva:\t" + FOUNDED + "\n"
+                + "\t Elnök\t\t: " + president + "\n"
+                + "==============================\n"
+                + source.getName() + " -> " + destination.getName() + "\n"
+                + "idő:\t\t" + time + " óra\n"
+                + priceInfo + "\n"
+                + "távolság:\t" + getDistance() + " km";
+    }
+}
