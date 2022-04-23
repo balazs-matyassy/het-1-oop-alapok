a24470173123ab461a0c11c3a52e4e63f605f08c
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-04-23 12:09:00 +0200

    mb: updates

--- src/main/java/hu/progmatic/tourism_02_wrapper/App2.java
+++ src/main/java/hu/progmatic/tourism_02_wrapper/App2.java
@@ -20,5 +20,34 @@ public class App2 {
         System.out.println("====================");
         System.out.println(moonEarth.getInfoSheet());
         System.out.println();
+
+        Integer cheapest = 2500;
+        Integer mostExpensive = 2500;
+
+        // == nem jó, mert az a referenciákat hasonlítja össze
+        if (cheapest.equals(mostExpensive)) {
+            System.out.println("Csak egyféle ár van.");
+        }
+
+        // unboxing
+        // int cheapestP = cheapest.intValue();
+        int cheapestP = cheapest;
+        cheapestP -= 1000;
+
+        // autoboxing
+        // cheapest = Integer.valueOf(cheapestP);
+        cheapest = cheapestP;
+
+        // miért nem az eredeti objektum értékét állítjuk?
+        // immutable
+        Integer balance = 1000;
+        // bank.setBalance(balance);
+
+        // ha nem lenne immutable,
+        // akkor utólag a bank tudta nélkül meg lehetne változtatni az egyenleget
+        // balance.setValue(100_000_000);
+        // mivel csak referencia szintjén adtam át a banknak, emiatt megváltozna az érték
+
+        Integer unknown = null;
     }
 }
